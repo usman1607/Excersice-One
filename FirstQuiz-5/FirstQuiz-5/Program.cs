86e6e34e94b6149d29cd6189e2586f8a4b7389c2
usman1607 <usman1607@yahoo.com>
2021-04-26 23:43:51 +0100

    new commit

---
+++ FirstQuiz-5/FirstQuiz-5/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+//Question 5: Write a C# program to reverse the words of a sentence.
+
+
+using System;
+
+namespace FirstQuiz_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string originalSentence = "The name of the boy is John Alex";
+            string[] words = originalSentence.Split(" ");                   //Split the sentence to string array of words.
+            Array.Reverse(words);                                      //Reverse the words in the array.
+            string newSentence = String.Join(" ", words);       //Form another sentence with the reversed words.
+
+            Console.WriteLine(newSentence);
+        }
+    }
+}
---
+++ FirstQuiz-9/FirstQuiz-9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FirstQuiz_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RemoveChar("International", 6));
+        }
+
+        static string RemoveChar(string word, int x)
+        {
+            return word.Remove(x, 1);   //Remove character at index x alone from word.
+                                                //You can parse 3 in place of 1 to remove 3 character from index at x.
+        }
+    }
+}
